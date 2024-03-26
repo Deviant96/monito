7031a5a85d0de9c69c7c6eb9fbd30217581d4a0e
deviant96 <mcrealdeviant@gmail.com>
2024-03-27 03:01:06 +0700

    Cta Section responsive update

--- src/app/(components)/CtaSection/page.js
+++ src/app/(components)/CtaSection/page.js
@@ -5,17 +5,19 @@ import styles from "./page.module.scss";
 const CtaSection = () => {
     return (
         <section className={styles.CtaSection}>
-            <header className="">
-                <h1>One more friend</h1>
-                <h2>Thousands more fun!</h2>
-            </header>
-            <div className={styles.textContent}>
-                Having a pet means you have more joy, a new friend, a happy person who will always be with you to have fun. We have 200+ different pets that can meet your needs!
+            <div className={styles.textSection}>
+                <header>
+                    <h1>One more friend</h1>
+                    <h2>Thousands more fun!</h2>
+                </header>
+                <div className={styles.textContent}>
+                    Having a pet means you have more joy, a new friend, a happy person who will always be with you to have fun. We have 200+ different pets that can meet your needs!
+                </div>
+                <footer className={styles.ctaContainer}>
+                    <a className={styles.primaryButton} href="/intro">View Intro <FaRegCirclePlay /></a>
+                    <a className={styles.secondaryButton} href="/explore">Explore Now</a>
+                </footer>
             </div>
-            <footer className={styles.ctaContainer}>
-                <a className={styles.primaryButton} href="/intro">View Intro <FaRegCirclePlay /></a>
-                <a className={styles.secondaryButton} href="/explore">Explore Now</a>
-            </footer>
 
             <div className={styles.ctaImageContainer}>
                 <Image className={styles.ctaImage} src="/ctaImage.png" alt="A woman lifting a dog" width={200} height={300} />
