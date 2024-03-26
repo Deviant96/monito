3cb3a80de854229219b1e0e8662f7ddecf153bef
Miretazam Ciptaprima <mcrealdeviant@gmail.com>
2024-03-26 12:04:12 +0700

    Move label

--- src/app/(components)/CtaSubscribeSection/page.js
+++ src/app/(components)/CtaSubscribeSection/page.js
@@ -7,8 +7,8 @@ const CtaSubscribeSection = () => {
                 <h1>Register now so you don&apos;t miss our programs</h1>
             </header>
             <footer className={styles.Footer}>
-                <label for="ctaSubscribe">Enter your email</label>
                 <form>
+                    <label for="ctaSubscribe">Enter your email</label>
                     <input id="ctaSubscribe" type="text" placeholder="Enter your email" />
                     <input type="submit" value="Subcribe Now" />
                 </form>
